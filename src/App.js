e850ad254ee16b7bcda3a5126218aef900d07413
Boris Etingof <boris.etingof@reckon.com>
2018-11-14 11:57:07 +1100

    wip

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Lenka Web
+            Lenka
           </a>
         </header>
       </div>
